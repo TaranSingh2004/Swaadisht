af2fded3153b4aed22353a3b43b3bce17ee8d84b
TaranSingh2004 <taranduggal2004@gmail.com>
2025-04-07 09:26:57 +0530

    Message handling

--- src/main/java/co/swaadisht/swaadisht/Controller/HomeController.java
+++ src/main/java/co/swaadisht/swaadisht/Controller/HomeController.java
@@ -3,6 +3,9 @@ package co.swaadisht.swaadisht.Controller;
 import co.swaadisht.swaadisht.Services.UserService;
 import co.swaadisht.swaadisht.entities.User;
 import co.swaadisht.swaadisht.forms.UserForm;
+import co.swaadisht.swaadisht.helpers.Message;
+import co.swaadisht.swaadisht.helpers.MessageType;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,18 +39,24 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm){
+    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session){
 //        System.out.println(userForm);
-        User user = User.builder()
-                .name(userForm.getName())
-                .email(userForm.getEmail())
-                .password(userForm.getPassword())
-                .phoneNumber(userForm.getPhoneNumber())
-                .profilePic("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fdefault-profile-picture&psig=AOvVaw1iyEPc3lJJRtMa74rzjCaP&ust=1744055862434000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJj6yseYxIwDFQAAAAAdAAAAABAE")
-                .build();
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setEnabled(false);
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fdefault-profile-picture&psig=AOvVaw1iyEPc3lJJRtMa74rzjCaP&ust=1744055862434000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJj6yseYxIwDFQAAAAAdAAAAABAE");
+
         User savedUser = userService.saveUser(user);
+
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+        session.setAttribute("message", message);
+
         System.out.println("User saved");
         System.out.println(savedUser);
+
         return "redirect:/register";
     }
 
